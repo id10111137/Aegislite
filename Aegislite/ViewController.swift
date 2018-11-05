bbee10dc668c09f733848193701b7a012e40a74a
Tatang Roswandi Ganda Wijaya <id.10111137@gmail.com>
2018-11-05 14:02:56 +0700

    Initial Commit

---
+++ Aegislite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aegislite
+//
+//  Created by Tatang Roswandi Ganda Wijaya on 05/11/18.
+//  Copyright © 2018 Tatang Roswandi Ganda Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
